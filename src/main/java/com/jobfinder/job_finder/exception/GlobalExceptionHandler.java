2b40ce42edcdde9aedd39041082b75a7ad0e59b0
DucLong64 <longld.b21cn494@stu.ptit.edu.vn>
2025-05-10 15:20:52 +0700

    loại bỏ Bean của globalException

--- src/main/java/com/jobfinder/job_finder/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jobfinder/job_finder/exception/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+
 public class GlobalExceptionHandler {
 
     // Xử lý mọi ngoại lệ chung
