d54cab798eebb331327b8c54ce8c759e40d3d24e
DucLong64 <longld.b21cn494@stu.ptit.edu.vn>
2025-05-19 16:54:45 +0700

    update search API for Seeker

--- src/main/java/com/jobfinder/job_finder/controller/SeekerController.java
+++ src/main/java/com/jobfinder/job_finder/controller/SeekerController.java
@@ -4,13 +4,11 @@ import com.jobfinder.job_finder.dto.JobPostingDTO;
 import com.jobfinder.job_finder.dto.response.ApiResponse;
 import com.jobfinder.job_finder.dto.response.ShiftDTO;
 import com.jobfinder.job_finder.service.JobPostingService;
+import com.jobfinder.job_finder.service.SeekerService;
 import com.jobfinder.job_finder.service.ShiftService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,8 @@ public class SeekerController {
     private ShiftService shiftService;
     @Autowired
     private JobPostingService jobPostingService;
+    @Autowired
+    private SeekerService seekerService;
     @GetMapping("/schedule/{seekerId}")
     public ResponseEntity<ApiResponse<?>> getSchedule(@PathVariable Long seekerId) {
         List<ShiftDTO> shitfs = shiftService.getScheduleForSeeker(seekerId);
@@ -31,6 +31,12 @@ public class SeekerController {
         List<JobPostingDTO> jobPostings = jobPostingService.getAllOpenJobPostings();
         ApiResponse<List<JobPostingDTO>> apiResponse = new ApiResponse<>(200,"Success", jobPostings);
         return ResponseEntity.ok(apiResponse);
+    }
+
+    @GetMapping ("/jobs/search")
+    public ResponseEntity<ApiResponse<List<JobPostingDTO>>> getJobPostings(@RequestParam String search) {
+        List<JobPostingDTO> jobPostingDTOS = seekerService.searchForSeeker(search);
+        return ResponseEntity.ok(new ApiResponse<>(200,"Success",jobPostingDTOS));
     }
 
 }
--- src/main/java/com/jobfinder/job_finder/repository/JobPostingRepository.java
+++ src/main/java/com/jobfinder/job_finder/repository/JobPostingRepository.java
@@ -20,4 +20,6 @@ public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
     // Mới: tìm theo recruiterId, năm và tháng
     @Query("SELECT j FROM JobPosting j WHERE j.recruiter.id = :recruiterId AND FUNCTION('YEAR', j.postDate) = :year AND FUNCTION('MONTH', j.postDate) = :month")
     List<JobPosting> findByRecruiterIdAndYearAndMonth(@Param("recruiterId") Long recruiterId, @Param("year") Integer year, @Param("month") Integer month);
+
+    List<JobPosting> findByTitleContainingIgnoreCaseAndStatus(String title, JobStatus status);
 }
